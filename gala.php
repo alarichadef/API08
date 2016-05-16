18c35a109e1a3413c0cabb79d0fe63d68b1d78a7
alarichadef <alarichadef@gmail.com>
2016-05-16 15:45:29 +0200

    Remove text-muted

--- gala.php
+++ gala.php
@@ -20,6 +20,20 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/font-awesome.min.css">
 	<link rel="stylesheet" href="css/style.css">
+<style>  
+.myhred{
+height: 5px;
+margin-left: auto;
+margin-right: auto;
+background-color:#FF0066;
+color:#FF0066;
+border: 0 none;
+margin-top: 50px;
+margin-bottom:50px;
+
+}
+
+</style>
 	<!--[if lt IE 9]>
 	<script src="js/html5shiv.js"></script>
 	<script src="js/respond.min.js"></script>
@@ -27,10 +41,35 @@
 </head>
 <body class="home">
 	<!-- Barre de navigation -->
-	<?php include('header.php');?>
+	<nav class="navbar navbar-inverse navbar-fixed-top navbar_center col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		<div class="container col-sm-offset-1 col-md-offset-3 col-lg-offset-4 ">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<div id="navbar" class="navbar-collapse collapse">
+				<ul class="nav navbar-nav">
+					<li class = "active"><a href="index.html">ACCUEIL</a></li>
+					<li><a href="index.html#association">L'ASSOCIATION</a></li>
+					<li><a href="index.html#utceenne">L'UTCEENNE</a></li>
+					<li><a href="index.html#gala">LE GALA</a></li>
+					<li><a href="index.html#contact">CONTACT</a></li>
+					<li><a class="btn btn-success" href="http://assos.utc.fr/etuville/billetterie/">BILLETTERIE</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
 	<!-- /Barre de navigation -->
 	<!-- Logo -->
-	<?php include('logo_header.php');?>
+	<header id="head">
+		<img id="logo" class="img-responsive center-block" src="images/logo.png" alt="Logo Etuville">
+	</header>
 	<!-- /Logo -->
 	<!-- Contenu -->
 	<div class="container">
@@ -38,14 +77,14 @@
 			<br><br>
 			<h2 class="thin text-center">Le Gala</h2>
 			<br><br>
-			<p class="text-muted">Le Gala de l’UTC, la soirée de prestige.<br><br>
+			<p>Le Gala de l’UTC, la soirée de prestige.<br><br>
 			Rendez-vous le XX XXXXXXXXXX 2016 à l’UTC pour le Gala, qui pour cette année, a été totalement repensé.<br><br>
 			Pour cette édition, l’équipe d’Etuville vous accueille dans les locaux de l’UTC au niveau de la nouvelle zone près des bords de l’Oise (Centre d’Innovation, Centre de Transfert...) Cette soirée en l’honneur des nouveaux diplômés réunira également les étudiants, les membres du personnel et leurs proches.<br><br>
 			Plus de détails prochainement...</p>
-			<p class="text-muted">
+			<p>
 				Et des photos là.
 			</p>
-			<p class="text-muted">
+			<p>
 				Et des vidéos là.
 			</p>
 		</div>
@@ -58,19 +97,19 @@
 					<a name="article1"></a>
 					<article>
 
-						<h1 class="text-muted" >FEDER sera présent au Gala !</a></h1>
+						<h1 class="text-muted">FEDER sera présent au Gala !</a></h1>
 									<hr>
 
 									<img src="http://placehold.it/900x300" class="img-responsive">
 
 									<br />
 
-									<p class="text-muted">De son vrai nom Hadrien Federiconi, Feder est originaire de Nice. Il compte parmi les grands noms de la deep-house française, grâce à son titre «Goodbye ft Lyse» qui connait en ce moment un gros succès dans toute l'Europe.
+									<p>De son vrai nom Hadrien Federiconi, Feder est originaire de Nice. Il compte parmi les grands noms de la deep-house française, grâce à son titre «Goodbye ft Lyse» qui connait en ce moment un gros succès dans toute l'Europe.
 
 Ecoutez son single "Goodbye" !
 </p>
 
-									<p class="text-muted">The symbol of Aries is the Ram, and that's both good and bad news. Impulsive Aries might be tempted to ram their ideas down everyone's throats without even bothering to ask if they want to know. It's these times when you may wish Aries' symbol were a more subdued creature, more lamb than ram perhaps. You're not likely to convince the Ram to soften up; these folks are blunt and to the point. Along with those qualities comes the sheer force of the Aries nature, a force that can actually accomplish a great deal. Much of Aries' drive to compete and to win comes from its Cardinal Quality. Cardinal Signs love to get things going, and Aries exemplifies this even better than Cancer, Libra or Capricorn.</p>
+									<p>The symbol of Aries is the Ram, and that's both good and bad news. Impulsive Aries might be tempted to ram their ideas down everyone's throats without even bothering to ask if they want to know. It's these times when you may wish Aries' symbol were a more subdued creature, more lamb than ram perhaps. You're not likely to convince the Ram to soften up; these folks are blunt and to the point. Along with those qualities comes the sheer force of the Aries nature, a force that can actually accomplish a great deal. Much of Aries' drive to compete and to win comes from its Cardinal Quality. Cardinal Signs love to get things going, and Aries exemplifies this even better than Cancer, Libra or Capricorn.</p>
 									<hr>
 									<div class="embed-responsive embed-responsive-16by9">
 														<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/Mptdcx36qZU"></iframe>
@@ -82,16 +121,16 @@ Ecoutez son single "Goodbye" !
 					<a name="bruno"></a>
 					<article>
 
-						<h1 class="text-muted" >Exemple d'article sur la maman de Bruno</a></h1>
+						<h1 class="text-muted">Exemple d'article sur la maman de Bruno</a></h1>
 									<hr>
 
 									<img src="http://placehold.it/900x300" class="img-responsive">
 
 									<br />
 
-									<p class="text-muted">Aries is the first sign of the zodiac, and that's pretty much how those born under this sign see themselves: first. Aries are the leaders of the pack, first in line to get things going. Whether or not everything gets done is another question altogether, for an Aries prefers to initiate rather than to complete. Do you have a project needing a kick-start? Call an Aries, by all means. The leadership displayed by Aries is most impressive, so don't be surprised if they can rally the troops against seemingly insurmountable odds.</p>
+									<p>Aries is the first sign of the zodiac, and that's pretty much how those born under this sign see themselves: first. Aries are the leaders of the pack, first in line to get things going. Whether or not everything gets done is another question altogether, for an Aries prefers to initiate rather than to complete. Do you have a project needing a kick-start? Call an Aries, by all means. The leadership displayed by Aries is most impressive, so don't be surprised if they can rally the troops against seemingly insurmountable odds.</p>
 
-									<p class="text-muted">The symbol of Aries is the Ram, and that's both good and bad news. Impulsive Aries might be tempted to ram their ideas down everyone's throats without even bothering to ask if they want to know. It's these times when you may wish Aries' symbol were a more subdued creature, more lamb than ram perhaps. You're not likely to convince the Ram to soften up; these folks are blunt and to the point. Along with those qualities comes the sheer force of the Aries nature, a force that can actually accomplish a great deal. Much of Aries' drive to compete and to win comes from its Cardinal Quality. Cardinal Signs love to get things going, and Aries exemplifies this even better than Cancer, Libra or Capricorn.</p>
+									<p>The symbol of Aries is the Ram, and that's both good and bad news. Impulsive Aries might be tempted to ram their ideas down everyone's throats without even bothering to ask if they want to know. It's these times when you may wish Aries' symbol were a more subdued creature, more lamb than ram perhaps. You're not likely to convince the Ram to soften up; these folks are blunt and to the point. Along with those qualities comes the sheer force of the Aries nature, a force that can actually accomplish a great deal. Much of Aries' drive to compete and to win comes from its Cardinal Quality. Cardinal Signs love to get things going, and Aries exemplifies this even better than Cancer, Libra or Capricorn.</p>
 									<hr>
 									<div class="embed-responsive embed-responsive-16by9">
 														<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/BbQk78Skmb4"></iframe>
